abeab09ba3e39ade912d15a277081c7924c63e27
Samuel A. Falvo II <kc5tja@arrl.net>
2011-06-11 11:09:19 -0400

    Finished assembler's target memory accessors

---
+++ asm-test.f
@@ -0,0 +1,28 @@
+warning off
+marker ==end==
+include asm.f
+
+: fetch     t@ drop ;
+: t00.1     -1 ['] fetch catch 0= abort" t00.1 : negative address" ;
+: t00.2     16384 ['] fetch catch 0= abort" t00.2 : address out of bounds" ;
+: t00.3     0 ['] fetch catch abort" t00.3 : valid address" ;
+: t00.4     16382 ['] fetch catch abort" t00.4 : valid address" ;
+: t00.5     16383 ['] fetch catch 0= abort" t00.5 : one byte out of bounds" ;
+: t00       t00.1 t00.2 t00.3 t00.4 t00.5 ;
+t00
+
+: t01.1     1234 -1 ['] t! catch 0= abort" t01.1 : negative address" ;
+: t01.2     1234 16384 ['] t! catch 0= abort" t01.2 : address out of bounds" ;
+: t01.3     1234 0 ['] t! catch abort" t01.3 : valid address" ;
+: t01.4     1234 16382 ['] t! catch abort" t01.4 : valid address" ;
+: t01.5     1234 16383 ['] t! catch 0= abort" t01.5 : one byte out of bounds" ;
+: t01       t01.1 t01.2 t01.3 t01.4 t01.5 ;
+t01
+
+: t02.1     1234 0 t!  0 t@  1234 xor abort" t02.1 : t! t@ should preserve bit order" ;
+: t02       t02.1 ;
+t02
+
+==end==
+bye
+
---
+++ asm.f
@@ -0,0 +1,38 @@
+0 [if]  The J1 processor sports a 13-bit wide program counter, address-
+        ing cells, not bytes.  Hence, the processor can only execute
+        instructions from locations $0000 to $3FFE, inclusive.
+[then]
+
+16384 constant /image
+create image
+/image allot
+
+0 [if]  When assembling/compiling, repeated references into target memory
+        space will need to be made.  We want to ensure that we don't
+        spill over into host memory when doing so.  Accessing any cell
+        or byte not belonging to the target is strictly an error, and
+        will result in immediate termination of the compiler.
+
+        It should be noted that most contemporary Forth environments are
+        32-bit or 64-bit, not 16-bit.  Thus, to better emulate how the
+        J1 will be referencing memory, we would like to use @ and !, but
+        constrained to 16-bits wide.  ANS Forth provides no such support,
+        so we synthesize our own using C@ and C!, which we pray operate
+        on 8-bit units of memory.
+
+        To facilitate more seamless support for graphics, we define
+        byte address A+0 to be displayed prior to A+1.  Since, to the
+        human eye, A+0 appears to the left of A+1, we treat A+0 as
+        bits 15..8 of a 16-bit quantity.  In other words, we define
+        the J1 arbitrarily as big-endian.
+[then]
+
+: -small    dup 0< abort" Address out of range" ;
+: -big      dup /image >= abort" Address out of range" ;
+: -odd      dup 1 and abort" Attempt to access cell with unaligned address" ;
+: >host     -small -big image + ;
+: w@        dup c@ 8 lshift swap 1+ c@ 255 and or ;
+: w!        over 8 rshift over c! 1+ c! ;
+: t@        -odd >host w@ ;
+: t!        -odd >host w! ;
+
