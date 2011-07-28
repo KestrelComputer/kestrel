4173a7cecb4875c076ff1f6ffc407dbe72146288
Samuel A. Falvo II <kc5tja@arrl.net>
2011-07-28 14:51:36 -0600

    Generates everything, instead of package at a time.

--- tangle-loop.sh
+++ tangle-loop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 while true; do
+    notangle -Rks.c ks.nw >ks.c
     notangle -Rimg.f img.nw >img.f
     sleep 2
 done
--- weave-loop.sh
+++ weave-loop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 while true; do
+    noweave -index -html ks.nw >ks.html
     noweave -index -html img.nw >img.html
     sleep 2
 done
