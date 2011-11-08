14240e18a49c44fe35124c0b19e6e2a879577ed5
kc5tja@localhost.localdomain <kc5tja@localhost.localdomain>
2011-11-07 21:36:16 -0800

    Renovated the block-RAM memory module to more completely support the Wishbone bus interface.  Plus, I find it far easier to re-use than the older for-MGIA-use video memory module.

--- 2/nexys2/computer/M_mem.v
+++ 2/nexys2/computer/M_mem.v
@@ -22,39 +22,70 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module M_mem(
-	input  [13:1] ins_adr_i,
-	output [15:0] ins_dat_o,
-	input  [13:1] dat_adr_i,
-	output [15:0] dat_dat_o,
-	input  [15:0] dat_dat_i,
-	input         dat_we_i,
-	input         dat_stb_i,
-	output        dat_ack_o,
-	input         dat_clk_i,
-	input         dat_rst_i
+	input  [13:1]	ins_adr_i,
+	output [15:0]	ins_dat_o,
+	input				ins_cyc_i,
+	input				ins_stb_i,
+	output			ins_ack_o,
+
+	input  [13:1]	dat_adr_i,
+	output [15:0]	dat_dat_o,
+	input  [15:0]	dat_dat_i,
+	input				dat_we_i,
+	input				dat_cyc_i,
+	input				dat_stb_i,
+	output			dat_ack_o,
+
+	input				sys_clk_i,
+	input				sys_rst_i
 );
 
 	// Asynchronous address decoding logic.  Note that we
 	// need separate logic for the instruction and data
-	// buses.
-	wire ien0 = (ins_adr_i[13:11] == 3'b000);
-	wire ien1 = (ins_adr_i[13:11] == 3'b001);
-	wire ien2 = (ins_adr_i[13:11] == 3'b010);
-	wire ien3 = (ins_adr_i[13:11] == 3'b011);
-	wire ien4 = (ins_adr_i[13:11] == 3'b100);
-	wire ien5 = (ins_adr_i[13:11] == 3'b101);
-	wire ien6 = (ins_adr_i[13:11] == 3'b110);
-	wire ien7 = (ins_adr_i[13:11] == 3'b111);
+	// buses.  We need to qualify our enables with the x_CYC_I
+	// and x_STB_I signals to be fully Wishbone compatible.
+	wire ins_active = ins_cyc_i & ins_stb_i;
+	wire dat_active = dat_cyc_i & dat_stb_i;
 
-	wire den0 = (dat_adr_i[13:11] == 3'b000) & dat_stb_i;
-	wire den1 = (dat_adr_i[13:11] == 3'b001) & dat_stb_i;
-	wire den2 = (dat_adr_i[13:11] == 3'b010) & dat_stb_i;
-	wire den3 = (dat_adr_i[13:11] == 3'b011) & dat_stb_i;
-	wire den4 = (dat_adr_i[13:11] == 3'b100) & dat_stb_i;
-	wire den5 = (dat_adr_i[13:11] == 3'b101) & dat_stb_i;
-	wire den6 = (dat_adr_i[13:11] == 3'b110) & dat_stb_i;
-	wire den7 = (dat_adr_i[13:11] == 3'b111) & dat_stb_i;
+	// We need at least two clocks for the Xilinx block RAMs
+	// to emulate an asynchronous RAM.  Therefore, we need to
+	// introduce wait-states.  It's yucky, but Forth chips don't
+	// pipeline well, so it is what it is.
+	reg dat_ack;
+	reg ins_ack;
 
+	assign dat_ack_o = dat_ack;
+	assign ins_ack_o = ins_ack;
+
+	always @(posedge sys_clk_i) begin
+		dat_ack <= (~dat_ack & dat_active & ~sys_rst_i);
+		ins_ack <= (~ins_ack & ins_active & ~sys_rst_i);
+	end
+
+	// Each RAM needs its own set of enable signals, one for
+	// the instruction bus, and one for the data bus.
+	// They need to be qualified against their respective bus
+	// cycles.
+	wire ien0 = (ins_adr_i[13:11] == 3'b000) & ins_active;
+	wire ien1 = (ins_adr_i[13:11] == 3'b001) & ins_active;
+	wire ien2 = (ins_adr_i[13:11] == 3'b010) & ins_active;
+	wire ien3 = (ins_adr_i[13:11] == 3'b011) & ins_active;
+	wire ien4 = (ins_adr_i[13:11] == 3'b100) & ins_active;
+	wire ien5 = (ins_adr_i[13:11] == 3'b101) & ins_active;
+	wire ien6 = (ins_adr_i[13:11] == 3'b110) & ins_active;
+	wire ien7 = (ins_adr_i[13:11] == 3'b111) & ins_active;
+
+	wire den0 = (dat_adr_i[13:11] == 3'b000) & dat_active;
+	wire den1 = (dat_adr_i[13:11] == 3'b001) & dat_active;
+	wire den2 = (dat_adr_i[13:11] == 3'b010) & dat_active;
+	wire den3 = (dat_adr_i[13:11] == 3'b011) & dat_active;
+	wire den4 = (dat_adr_i[13:11] == 3'b100) & dat_active;
+	wire den5 = (dat_adr_i[13:11] == 3'b101) & dat_active;
+	wire den6 = (dat_adr_i[13:11] == 3'b110) & dat_active;
+	wire den7 = (dat_adr_i[13:11] == 3'b111) & dat_active;
+
+	// Each RAM chunk has its own set of data outputs, which
+	// we multiplex onto x_DAT_O as needed.
 	wire [15:0] iq0, iq1, iq2, iq3, iq4, iq5, iq6, iq7;
 	wire [15:0] dq0, dq1, dq2, dq3, dq4, dq5, dq6, dq7;
 
@@ -63,17 +94,7 @@ module M_mem(
 	assign ins_dat_o = ins_dat;
 	assign dat_dat_o = dat_dat;
 
-	// Gives processor the illusion that it's talking to an
-	// asynchronous RAM at 12.5MHz.  This implies that the
-	// data clock must run at 25.0MHz.
-	reg dat_ack;
-	assign dat_ack_o = dat_ack;
-	
-	always @(posedge dat_clk_i) begin
-		dat_ack <= (~dat_ack & dat_stb_i & ~dat_rst_i);
-	end
-
-	// RAM addressing logic
+	// RAM data bus multiplexing logic
 	always @* begin
 		case (ins_adr_i[13:11])
 			3'b000:	ins_dat <= iq0;
@@ -106,7 +127,7 @@ module M_mem(
 		.WEA(1'b0),						.WEB(dat_we_i),
 		.ENA(ien0),						.ENB(den0),
 		.SSRA(1'b0),					.SSRB(1'b0),
-		.CLKA(dat_clk_i),				.CLKB(dat_clk_i),
+		.CLKA(sys_clk_i),				.CLKB(sys_clk_i),
 		.ADDRA(ins_adr_i[10:1]),	.ADDRB(dat_adr_i[10:1]),
 		.DIA(16'h0000),				.DIB(dat_dat_i),
 		.DOA(iq0),						.DOB(dq0),
@@ -117,7 +138,7 @@ module M_mem(
 		.WEA(1'b0),						.WEB(dat_we_i),
 		.ENA(ien1),						.ENB(den1),
 		.SSRA(1'b0),					.SSRB(1'b0),
-		.CLKA(dat_clk_i),				.CLKB(dat_clk_i),
+		.CLKA(sys_clk_i),				.CLKB(sys_clk_i),
 		.ADDRA(ins_adr_i[10:1]),	.ADDRB(dat_adr_i[10:1]),
 		.DIA(16'h0000),				.DIB(dat_dat_i),
 		.DOA(iq1),						.DOB(dq1),
@@ -128,7 +149,7 @@ module M_mem(
 		.WEA(1'b0),						.WEB(dat_we_i),
 		.ENA(ien2),						.ENB(den2),
 		.SSRA(1'b0),					.SSRB(1'b0),
-		.CLKA(dat_clk_i),				.CLKB(dat_clk_i),
+		.CLKA(sys_clk_i),				.CLKB(sys_clk_i),
 		.ADDRA(ins_adr_i[10:1]),	.ADDRB(dat_adr_i[10:1]),
 		.DIA(16'h0000),				.DIB(dat_dat_i),
 		.DOA(iq2),						.DOB(dq2),
@@ -139,7 +160,7 @@ module M_mem(
 		.WEA(1'b0),						.WEB(dat_we_i),
 		.ENA(ien3),						.ENB(den3),
 		.SSRA(1'b0),					.SSRB(1'b0),
-		.CLKA(dat_clk_i),				.CLKB(dat_clk_i),
+		.CLKA(sys_clk_i),				.CLKB(sys_clk_i),
 		.ADDRA(ins_adr_i[10:1]),	.ADDRB(dat_adr_i[10:1]),
 		.DIA(16'h0000),				.DIB(dat_dat_i),
 		.DOA(iq3),						.DOB(dq3),
@@ -150,7 +171,7 @@ module M_mem(
 		.WEA(1'b0),						.WEB(dat_we_i),
 		.ENA(ien4),						.ENB(den4),
 		.SSRA(1'b0),					.SSRB(1'b0),
-		.CLKA(dat_clk_i),				.CLKB(dat_clk_i),
+		.CLKA(sys_clk_i),				.CLKB(sys_clk_i),
 		.ADDRA(ins_adr_i[10:1]),	.ADDRB(dat_adr_i[10:1]),
 		.DIA(16'h0000),				.DIB(dat_dat_i),
 		.DOA(iq4),						.DOB(dq4),
@@ -161,7 +182,7 @@ module M_mem(
 		.WEA(1'b0),						.WEB(dat_we_i),
 		.ENA(ien5),						.ENB(den5),
 		.SSRA(1'b0),					.SSRB(1'b0),
-		.CLKA(dat_clk_i),				.CLKB(dat_clk_i),
+		.CLKA(sys_clk_i),				.CLKB(sys_clk_i),
 		.ADDRA(ins_adr_i[10:1]),	.ADDRB(dat_adr_i[10:1]),
 		.DIA(16'h0000),				.DIB(dat_dat_i),
 		.DOA(iq5),						.DOB(dq5),
@@ -172,7 +193,7 @@ module M_mem(
 		.WEA(1'b0),						.WEB(dat_we_i),
 		.ENA(ien6),						.ENB(den6),
 		.SSRA(1'b0),					.SSRB(1'b0),
-		.CLKA(dat_clk_i),				.CLKB(dat_clk_i),
+		.CLKA(sys_clk_i),				.CLKB(sys_clk_i),
 		.ADDRA(ins_adr_i[10:1]),	.ADDRB(dat_adr_i[10:1]),
 		.DIA(16'h0000),				.DIB(dat_dat_i),
 		.DOA(iq6),						.DOB(dq6),
@@ -183,7 +204,7 @@ module M_mem(
 		.WEA(1'b0),						.WEB(dat_we_i),
 		.ENA(ien7),						.ENB(den7),
 		.SSRA(1'b0),					.SSRB(1'b0),
-		.CLKA(dat_clk_i),				.CLKB(dat_clk_i),
+		.CLKA(sys_clk_i),				.CLKB(sys_clk_i),
 		.ADDRA(ins_adr_i[10:1]),	.ADDRB(dat_adr_i[10:1]),
 		.DIA(16'h0000),				.DIB(dat_dat_i),
 		.DOA(iq7),						.DOB(dq7),
