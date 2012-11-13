d69658d06327f18598e399efb54a3f3400c86870
"kc5tja@arrl.net" <"kc5tja@arrl.net">
2012-11-13 09:25:11 -0800

    Finally got the SD card to fully initialize and auto-detect (well, the SDSC card at least; good enough for Forth Day!).  It's attempting to boot!  Except, reading blocks isn't implemented just yet, so naturally, it chokes.  Time to get some block reading done!

--- cores/Kestrel-2/sw/milestone-3.fs
+++ cores/Kestrel-2/sw/milestone-3.fs
@@ -13,9 +13,22 @@ int, invalid
 int, cardtype
 int, sector1
 int, dct
+int, max
 int, #read
 int, p
 int, q
+char, cmdbyte
+char, crcbyte
+int, paraml
+int, paramh
+char, out
+char, in
+char, _r1
+int, _r3h
+int, _r3l
+int, inw
+int, outw
+int, t.o.
 
 
 $0001 const, CARD_MMC
@@ -79,28 +92,11 @@ decimal
 
 :, -v1?		cardtype @, -CARD_V1 and, ;,
 :, v1|mmc?	cardtype @, CARD_V1MMC and, ;,
+:, v2hc?	cardtype @, CARD_V2HC and, ;,
 
 
 \ STUBS
 :, read1k	1023 #, #read !, ;,
-:, 50ms		;,
-
-:, cmd0		;,
-:, cmd1		;,
-:, cmd8		;,
-:, cmd16	;,
-:, acmd41	;,
-:, cmd58	;,
-
-:, -idle?	1 #, ;,
-:, idle?	-idle? 1 #, xor, ;,
-:, illegal?	0 #, ;,
-:, -ready?	1 #, ;,
-:, timeout?	0 #, ;,
-:, ccs?		0 #, ;,
-
-:, miso		gpip @, kMISO and, ;,
-
 
 
 :, clk0		gpop @, $FFF7 #, and, gpop !, ;,
@@ -111,9 +107,62 @@ decimal
 :, mosi1	mosi0  gpop @, 4 #, xor, gpop !, ;,
 :, cs1		cs0 gpop @, 2 xor, gpop !, ;,
 
+:, miso		gpip @, kMISO and, ;,
+
+
 :, sleep	dct @, -1 #, +, dct !,   dct @, if, again, then, ;,
+:, 50ms		1000 #, dct !, sleep ;,
 :, 5ms		100 #, dct !, sleep ;,
-:, clk		clk+- 5ms ;,
+:, clk		5ms clk+- ;,
+
+:, mosi		if, mosi1 ;, then, mosi0 ;,
+:, b		out c@, $80 #, and, mosi clk clk   out c@, out c@, +, out c!, ;,
+:, snd		b b b b b b b b ;,
+:, sndw		outw 1 #, +, c@, out c!, snd  outw c@, out c!, snd ;,
+:, send		cmdbyte c@, out c!, snd paramh @, outw !, sndw paraml @, outw !, sndw crcbyte c@, out c!, snd ;,
+:, pad		$FF #, out c!, snd ;,
+
+:, (wait)	max @, if,
+		  miso if, clk clk  -1 #, max @, +, max !, again, then,
+		  0 #, t.o. !, ;,
+		then, 1 #, t.o. !, ;,
+:, wait		8192 #, max !, mosi1 (wait) ;,
+:, issue	send wait ;,
+:, timeout?	t.o. @, ;,
+:, -timeout?	timeout? 1 #, xor, ;,
+
+:, b		in c@, in c@, +, in c!,  miso if, in c@, 1 #, xor, in c!, then,  clk clk ;,
+:, r1		0 #, in c!,  b b b b b b b b in c@, _r1 c!, ;,
+:, rcv		0 #, in c!,  b b b b b b b b ;,
+:, rcvw		rcv in c@, inw 1 #, +, c!,  rcv in c@, inw c!, ;,
+:, (r3)		rcvw inw @, _r3h !,  rcvw inw @, _r3l !, ;,
+:, r3		r1 _r1 c@, $7C #, and, if, ;, then, (r3) ;,
+:, r7		r3 ;,
+
+:, param0	0 #, paraml !, 0 #, paramh !, ;,
+:, cmd0		cs0 $40 #, cmdbyte c!, param0 $95 #, crcbyte c!, issue -timeout? if, r1 then, cs1 pad ;,
+:, cmd1		cs0 $41 #, cmdbyte c!, param0 issue -timeout? if, r1 then, cs1 pad ;,
+:, cmd8		cs0 $48 #, cmdbyte c!, param0 $01AA paraml !, issue -timeout? if, r7 then, cs1 pad ;,
+:, cmd16	cs0 $50 #, cmdbyte c!, 512 #, paraml !, 0 #, paramh !, issue -timeout? if, r1 then, cs1 pad ;,
+:, cmd55	cs0 $77 #, cmdbyte c!, param0 issue -timeout? if, r1 then, cs1 pad ;,
+:, hcs		v2hc? if, $4000 #, ;, then, 0 #, ;,
+:, acmd41	cmd55 cs0 $69 #, cmdbyte c!, hcs paramh !, 0 #, paraml !, issue -timeout? if, r1 then, cs1 pad ;,
+:, cmd58	cs0 $7A #, cmdbyte c!, param0 issue -timeout? if, r3 then, cs1 pad ;,
+
+\ :, -idle?	1 #, ;,
+\ :, idle?	-idle? 1 #, xor, ;,
+\ :, illegal?	0 #, ;,
+\ :, -ready?	1 #, ;,
+\ :, timeout?	0 #, ;,
+\ :, ccs?		0 #, ;,
+
+:, idle?	_r1 c@, $01 #, and, ;,
+:, -idle?	idle? 1 #, xor, ;,
+:, illegal?	_r1 c@, $04 #, and, ;,
+:, -illegal?	illegal? 1 #, xor, ;,
+:, -ready?	_r1 c@, ;,
+:, ccs?		_r3h @, $4000 #, and, ;,
+
 :, 10cyc	clk clk clk clk clk  clk clk clk clk clk ;,
 :, 80cyc	10cyc 10cyc 10cyc 10cyc  10cyc 10cyc 10cyc 10cyc ;,
 :, stabilize	clk0 mosi1 cs1 80cyc ;,
@@ -121,28 +170,29 @@ decimal
 
 :, -valid 	1 #, invalid !, ;,
 
-: to. 		timeout? if, -valid ;, then, ;
+: to. 		timeout? if, -valid $8001 #, invalid !, ;, then, ;
 : valid		invalid @, if, ;, then, ;
 
-:, size		valid  cmd16 to.  -ready? if, -valid then, ;,
+:, size		valid  cmd16 to.  -ready? if, -valid $A001 #, invalid !, then, ;,
 :, bytes	512 #, sector1 !, ;,
 :, sdhc		CARD_HC cardtype !,    1 #, sector1 !, ;,
 :, sdsc		CARD_V2 cardtype !,  bytes ;,
-:, check	cmd58 to.  -ready? if, -valid ;, then,  ccs? if, sdhc ;, then,  sdsc ;,
+:, check	cmd58 to.  -ready? if, -valid $E001 #, invalid !, ;, then,  ccs? if, sdhc ;, then,  sdsc ;,
 :, addressing	valid  v1|mmc? if, bytes ;, then, check ;,
 :, cmd		cardtype @, CARD_MMC xor, if, acmd41 ;, then, cmd1 ;,
 :, poll		cmd to. ;,
 :, wait		valid  50ms  poll
                 illegal? if,
-		  -v1? if, -valid ;, then,
+		  cardtype @, CARD_V2HC and, if, $98F1 #, invalid !, ;, then,
+		  -v1? if, -valid $98A1 #, invalid !, ;, then,
                   v1|mmc? if, CARD_MMC cardtype !, again, then,
 		then,
 		idle? if, again, then,
-		-ready? if, -valid then, ;,
-:, verify	valid  cmd58 to.  illegal? if, -valid then, ;,
+		-ready? if, -valid $9801 #, invalid !, then, ;,
+:, verify	valid  cmd58 to.  illegal? if, -valid $0C01 #, invalid !, then, ;,
 :, kind		illegal? if, CARD_V1MMC ;, then, CARD_V2HC ;,
 :, v1v2		valid  cmd8 to. kind cardtype !, ;,
-:, controller	cmd0 to.  -idle? if, -valid then, ;,
+:, controller	cmd0 to.  -idle? if, -valid $FF01 #, invalid !, then, ;,
 :, initialize	0 #, cardtype !,  0 #, invalid !, stabilize
 		miso if, controller v1v2 verify wait addressing size ;, then, -valid $FF #, invalid !, ;,
 
@@ -160,13 +210,13 @@ decimal
 :, wait...	off sd p !, icon wait ;,
 
 :, start	off cls 15360 #, go, ;,
-:, boot...	read1k  present if, #read @, 1024 #, xor, if, bad... then, start then, wait... ;,
+:, boot...	read1k  present if, #read @, 1024 #, xor, if, $AAAA #, invalid !, bad... then, start then, wait... ;,
 
-:, bad		present if, invalid @, $C000 #, !, again, then, wait... ;,
+:, bad		present if, invalid @, $C000 #, !,  _r1 c@, $C002 #, c!,  _r1 c@, -1 #, xor, $C052 #, c!, again, then, wait... ;,
 :, b		off sdx p !, icon bad ;,
 ' b >body @ is, bad...
 
-:, val		on cls initialize  present if, invalid @, if, bad... then, boot... then, wait... ;,
+:, val		on cls initialize  present if, invalid @, if, $CCCC #, invalid !, bad... then, boot... then, wait... ;,
 ' val >body @ is, validate...
 
 :, power...	cls  present if, validate... then, wait... ;,
