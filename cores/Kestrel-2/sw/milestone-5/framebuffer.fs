39dfc5f474f666d4f08493bc9a3227e080f35054
"kc5tja@arrl.net" <"kc5tja@arrl.net">
2013-02-13 23:11:27 -0800

    Completed BlackRect procedure.

--- cores/Kestrel-2/sw/milestone-5/framebuffer.fs
+++ cores/Kestrel-2/sw/milestone-5/framebuffer.fs
@@ -1,8 +1,92 @@
+\ Dependencies:
+\ 
+\ Character Map Descriptor Accessor Words
+\ 
+\ These words are accessors, meaning they fetch the relevant fields from a
+\ (logical) data structure called the Character Matrix Descriptor, or CMD.
+\ The CMD tells the frame buffer logic how many characters across the frame
+\ buffer is, and how many characters down.  It additionally tells the code
+\ where to find the bitplane(s) for the frame buffer, so that drawing can
+\ occur.
+\ 
+\ Note that the CMD is intrinsically hardware and framebuffer logic
+\ dependent!  If you change video hardware, you'll almost certainly need
+\ new framebuffer and CMD code to describe them.  The CMD's value becomes
+\ apparent when you have closely-related classes of hardware.  For example,
+\ this code can reasonably be expected to work (with small modifications)
+\ on an Atari ST or Commodore-Amiga, for they both rely on bitplanes like the
+\ Kestrel.  However, a 320x240 Mode-X VGA display will be fundamentally in-
+\ compatible, and will require a completely different framebuffer driver
+\ implementation.  It's likely that driver will need a slightly different set
+\ of fields for the CMD as well, as mode-X VGA does not rely on bitplanes.
+\ 
+\ bitplane ( -- a )	Pointer to the bitplane memory.  This field is hardware
+\			dependent.
+\ #ch/row ( -- n )	Number of characters per row.  Assumes a fixed-width font.
+\ #rows ( -- n )	Number of rows that can fit completely in the bitplane.
+\ 			Partial rows are not supported.
+\ #px/row ( -- n )	Number of raster-lines (pixels) per row.
+\ mt/rows ( -- a )	Pointer to a table that maps row number to byte offset
+\			into the bitplane.  At least #rows entries must exist.
+\ mt/px ( -- a )	Pointer to a table that maps raster-line to byte offset
+\			within a single row.  At least #px/row entries must exist.
+\ 
+\ Using this framebuffer code to render into off-screen bitmaps will require
+\ a configuration that supports multiplexing between different CMDs.  A CMD must
+\ exist for the screen, of course; in addition, all off-screen buffers must have
+\ their own CMDs as well.
+\ 
+\ Frame Buffer Control Block Locators
+\ 
+\ The frame buffer drawing code requires space in memory to accept parameters,
+\ and to maintain state as it completes its requested procedures.  This space
+\ exists in a Frame Buffer Control Block, or FBCB.  This chunk of memory, like
+\ the CMD above, is logical from the perspective of the framebuffer code.
+\ Thus, support for multiple FBCBs or a single, global FBCB is determined through
+\ external configuration.
+\ 
+\ These words are locators, for they return addresses to memory cells used to
+\ hold state.  Some state is public, that is, intended to be used by application
+\ software.  Other state is private in nature, and is not intended to be
+\ utilized directly by any software except for the framebuffer code itself.
+\ These private fields are documented, however, so that in a multi-FBCB system,
+\ adequate space for these private fields can be reserved in each FBCB.
+\ 
+\ Left		The left-hand edge of a bounding rectangle.
+\ Top		The top edge of a bounding rectangle.
+\ Right		The right-hand edge of a bounding rectangle.
+\ Bottom	The bottom edge of a bounding rectangle.
+\ ErrFlag	This field always represents any error condition that prevented
+\		the framebuffer code from completing the procedure.  The inter-
+\		pretation of this field depends entirely upon the procedure
+\		invoked.
+\ 
+\ The following fields are private: p, w, y, and r.
+\ 
+\ BEWARE: Make sure none of the aforelisted FBCB fields refer to previously
+\ defined fields, or the framebuffer code will almost certainly crash.  They
+\ _must_ correspond, in essence, to full cell-sized variables.
+
+
 \ Constants useful for range checking
 ' #ch/row >body @ negate 1- const, -W-1    ( used for checking if x <= #ch/row )
 ' #rows >body @ negate 1- const, -H-1    ( used for checking if y <= #rows )
 
-:, (br)		;,
+\ Code to draw a black, character-aligned rectangle
+:, incy		y @, 1 #, +, y !, ;,
+:, decw		w @, -1 #, +, w !, ;,
+:, incp		p @, 1 #, +, p !, ;,
+:, stz		0 #, p @, 1 #, xor, c!, ;,  ( 1 #, xor, because of way MGIA maps bytes versus words in Kestrel-2 )
+:, blackr	w @, if, stz incp decw again, then, ;,
+:, edge		y @, y @, +,  mt/rows +, @,
+		r @, r @, +,  mt/px +, @, +,
+		bitplane +, Left @, +, p !, ;,
+:, width	Left @, -1 #, xor, 1 #, +, Right @, +, w !, ;,
+:, clrr		edge width blackr ;,
+:, crs		r @, #px/row xor, if, clrr r @, 1 #, +, r !, again, then, ;,
+:, clrrow	0 #, r !,  crs ;,
+:, allrows	y @, Bottom @, xor, if, clrrow incy again, then, ;,
+:, (br)		Top @, y !, allrows ;,
 
 \ Error conditions preventing BlackRect from working
 :, L>W		ErrFlag @, 1 #, xor, ErrFlag !, ;,
@@ -13,6 +97,8 @@
 :, T>B		ErrFlag @, 32 #, xor, ErrFlag !, ;,
 
 \ Guards against above-mentioned errors
+\ Notice that, in L<=R and T<=B, I do not add one to the negated value of Right
+\ or Bottom.  Since -X-1 = (~X+1)-1 = ~X, we leave off the adjustment for brevity.
 :, L<=W		Left @, -W-1 +, $8000 #, and, if, exit, then, L>W ;,
 :, R<=W		Right @, -W-1 +, $8000 #, and, if, exit, then, R>W ;,
 :, L<=R		Right @, -1 #, xor, Left @, +, $8000 #, and, if, exit, then, L>R ;, 
@@ -22,5 +108,5 @@
 :, validate	0 #, ErrFlag !,  L<=W R<=W L<=R  T<=H B<=H T<=B ;,
 
 \ Public API
-:, BlackRect	validate  ErrFlag @, if, exit, then, (br) ;,
+:, BlackRect	validate  ErrFlag @, if, exit, then,  (br) ;,
 
--- cores/Kestrel-2/sw/milestone-5/test-framebuffer.fs
+++ cores/Kestrel-2/sw/milestone-5/test-framebuffer.fs
@@ -45,7 +45,9 @@ int, r			( current raster counter )
 
 
 \ Module Under Test
+pibptr @
 include framebuffer.fs
+pibptr @ swap - . .( bytes in module compiled) cr
 
 
 \ Re-used and useful phrases
@@ -64,9 +66,11 @@ int, test
 \ Edge case negative tests (remaining tests serve as positive tests)
 
 :, =AAAA	p @, @, $AAAA #, xor, if, fail then, ;,
-:, (screen0)	p @, gap xor, if, =AAAA incp again, then, ;,
+:, (scrAA)	p @, gap xor, if, =AAAA incp again, then, ;,
+:, (scr00)	p @, gap xor, if, p @, @, if, fail then, incp again, then, ;,
 :, (gap0)	p @, if, =AAAA incp again, then, ;,
-:, scrAA	bitplane p !, (screen0) ;,
+:, scrAA	bitplane p !, (scrAA) ;,
+:, scr00	bitplane p !, (scr00) ;,
 :, gapAA	gap p !, (gap0) ;,
 :, (setup0)	p @, if, $AAAA #, p @, !, incp again, then, ;,
 :, setup	bitplane p !, (setup0) ;,
@@ -80,14 +84,37 @@ int, test
 :, t60		$0067 #, test !,  setup #ch/row 1 #, +, Left !, 0 #, Top !, #ch/row Right !,  #rows Bottom !,  BlackRect  ErrFlag @, $01 #, and, t ;,
 
 \ Zero-width tests
+
+:, noerr	ErrFlag @, if, ErrFlag @, $C00A #, !, ErrFlag @, -1 #, xor, $C05A #, !, fail then, ;,
+:, t		noerr scrAA gapAA ;,
+:, t100		$0107 #, test !,  setup  0 #, Left !,  0 #, Top !,  0 #, Right !,  #rows Bottom !,  BlackRect  t ;,
+:, t110		$0117 #, test !,  setup  10 #, Left !, 0 #, Top !,  10 #, Right !, #rows Bottom !,  BlackRect  t ;,
+:, t120		$0127 #, test !,  setup  #ch/row Left !,  0 #, Top !,  #ch/row Right !,  #rows Bottom !,  BlackRect  t ;,
+
 \ Zero-height tests
+:, t200		$0207 #, test !,  setup  0 #, Left !,  0 #, Top !,  #ch/row Right !,  0 #, Bottom !,  BlackRect  t ;,
+:, t210		$0217 #, test !,  setup  0 #, Left !, 10 #, Top !,  #ch/row Right !,  10 #, Bottom !,  BlackRect  t ;,
+:, t220		$0227 #, test !,  setup  0 #, Left !, #rows Top !,  #ch/row Right !,  #rows Bottom !,  BlackRect  t ;,
+
 \ Positive tests
+:, 1glyph00	$C000 #, @, $00AA #, xor, if, fail then,		( only a single glyph should be cleared )
+                $C050 #, @, $00AA #, xor, if, fail then,
+                $C0A0 #, @, $00AA #, xor, if, fail then,
+                $C0F0 #, @, $00AA #, xor, if, fail then,
+                $C140 #, @, $00AA #, xor, if, fail then,
+                $C190 #, @, $00AA #, xor, if, fail then,
+                $C1E0 #, @, $00AA #, xor, if, fail then,
+                $C230 #, @, $00AA #, xor, if, fail then, ;,
+
+:, t300		$0300 #, test !,  setup  0 #, Left !,  0 #, Top !,  1 #, Right !,  1 #, Bottom !,  BlackRect  noerr 1glyph00 gapAA ;,
+:, t310		$0317 #, test !,  setup  0 #, Left !,  0 #, Top !,  #ch/row Right !,  #rows Bottom !,  BlackRect  noerr scr00 gapAA ;,
+
 
 :, all-tests
 	t10 t20 t30 t40 t50 t60
-\	t100 t110 t120
-\	t200 t210 t220
-\	t300 t310
+	t100 t110 t120
+	t200 t210 t220
+	t300 t310
 	0 #, test !, fail ;,
 
 ' all-tests >body @ is, entry-point
