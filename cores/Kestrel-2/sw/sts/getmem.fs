1aa078dfcb76e447a1f7db84bde5f5a8c58ae117
Samuel A. Falvo II <kc5tja@arrl.net>
2013-08-30 00:02:45 -0700

    Add getmem, relmem, and fmtmem functions.
    Dynamic memory allocation is going to be important for STS, even with as little
    as 16KB of memory.  Defragmenting memory is also important, as we don't want to
    waste memory due to accumulation of chunks too small to use.
    
    When STS runs a program, it may bring in dependent libraries using the segment
    loader as well.  Many programs can depend on the same libraries; as a result,
    these libraries can be loaded at potentially different addresses.  To help keep
    track of where these libraries are placed, STS relies on dynamically allocated
    memory.  That's why getmem, relmem, and fmtmem functions exist.
    
    How to use:
    
    First you must format your memory pool.
    
    	pool p !
    	/pool reqsize !
    	fmtmem
    
    This creates the initial data structures required by getmem and relmem later.
    The name isn't a joke -- formatting the memory pool will, as with formatting a
    disk, destroy any previous allocations in the pool, so make sure you invoke it
    knowing what you're doing.
    
    Acquiring memory is possible using getmem:
    
    	pool poolbase !		(if not already done elsewhere)
    	/region reqsize !
    	getmem
    	result @ if ( handle error here ) then
    	p @ memptr !
    
    Make sure to release it again with relmem:
    
    	memptr @ p !
    	relmem
    
    Note that defragmentation happens on allocation, not on free.  Hence, getmem
    should not be used in real-time situations.  Pre-allocate your resources under
    those circumstances.

---
+++ cores/Kestrel-2/sw/sts/getmem.fs
@@ -0,0 +1,64 @@
+\ getmem.fs
+\ 
+\ These routines provide a dynamic memory management facility, allowing
+\ applications to request blocks of memory of a given size, and to release it
+\ later if required.
+\ 
+\ Pools are maintained through a singly linked, circular list of nodes with the
+\ invariant that a node N can point to node M if and only if M appears at a higher
+\ address than N, and which follows N immediately.  So, if looking at a memory
+\ dump of three nodes, A, B, and C in that order, A generally cannot point directly
+\ to C.  The sole exception is if A and B both represent free chunks of memory, where
+\ B's space coalesces with A to reduce fragmentation.
+\ 
+\ The structure of a node follows (offsets in cells):
+\ 
+\ +0	next		Pointer to next node in the list, or back to first node.
+\ 
+\ +1	--111111..	The size of the allocation, in bytes, excluding the header.
+\ 			Note that memory may not be requested in units smaller than
+\			four bytes.
+\ 
+\ +1	--......1.	Unused bit.  Must be set to zero on writing, and ignored on
+\			read.
+\ 
+\ +1	--.......1	1=block is allocated; 0=block is free.
+
+-2 cells constant -/node
+ 2 cells constant /node
+/node 1- constant roundup
+
+\ fmtmem formats a memory pool with the required metadata to support allocation requests.
+\ reqsize holds how big the pool is in bytes, and p points to its first byte.
+\ 
+\ Beware: formatting a pool that's been previously used will, in effect, forcefully de-
+\ allocate everything from that pool.
+
+: fmtmem	p @ p @ !  reqsize @ -/node and -/node + p @ cell+ ! ;
+
+\ getmem allocates reqsize bytes from the system memory pool.  If it can be satisfied,
+\ p holds a pointer to the block, result is zero, and reason is undefined.
+\ Otherwise, p remains undefined, result is non-zero, and reason provides some indication
+\ why.
+
+  : gmsplit	p @ @  p @ /node + reqsize @ + !
+		p @ /node + reqsize @ +  p @ !
+		p @ cell+ @  /node - reqsize @ -  p @ @ cell+ !
+		reqsize @ 1 or p @ cell+ ! ;
+
+  : gmstep	p @ cell+ @ 1 and if p @ @ p ! exit then
+		BEGIN  p @ @ cell+ @ 1 and 0=  p @ @ @ poolbase @ = 0= and
+		WHILE  p @ @ cell+ @ /node + p @ cell+ +!
+		       p @ @ @ p @ !
+		REPEAT
+		p @ cell+ @ reqsize @ U< if p @ @ p ! exit then
+		gmsplit  /node p +!  0 result ! ;
+
+: getmem	EBASE reason !  -1 result !  poolbase @ 0= if exit then
+		ESIZE reason !  poolbase @ p !  reqsize @ roundup + -/node and reqsize !
+		BEGIN gmstep result @ 0= p @ poolbase @ = or UNTIL ;
+
+\ relmem releases a chunk of memory allocated by getmem.
+
+: relmem	-/node p +!  p @ cell+ @  -/node and p @ cell+ !  0 result ! ;
+
---
+++ cores/Kestrel-2/sw/sts/test-getmem.fs
@@ -0,0 +1,87 @@
+\ test-getmem.fs
+\ 
+\ These routines provide a dynamic memory management facility, allowing
+\ applications to request blocks of memory of a given size, and to release it
+\ later if required.
+
+
+include errors.fs
+
+
+8192 constant /pool
+create pool	/pool allot
+
+\ Memory Request Block fields
+variable p
+variable reqsize
+variable result
+variable reason
+variable poolbase
+
+include getmem.fs
+
+\ Memory pool metadata itself requires storage.
+\ This prevents one from allocating the pool as a whole.
+
+: 0=		if 0 exit then -1 ;
+
+: s		pool p !  /pool reqsize !  fmtmem ;
+: t100.1	s  /pool reqsize !  getmem  result @ 0= abort" t100.1" ;
+: t100.2	s  0 poolbase !  /pool reqsize !  getmem  reason @ EBASE xor abort" t100.2" ;
+: t100.3	s  pool poolbase !  /pool reqsize !  getmem  reason @ ESIZE xor abort" t100.3" ;
+
+\ Allocating something smaller than the whole pool should
+\ succeed, however.
+
+: s		pool p !  /pool reqsize !  fmtmem  pool poolbase ! ;
+: t110.1	s  4000 reqsize !  getmem  result @ abort" t110.1" ;
+: t110.2	s  4000 reqsize !  getmem  p @ pool /node + xor abort" t110.2" ;
+: t110.3	s  4000 reqsize !  getmem getmem result @ abort" t110.3" ;
+: t110.4	s  4000 reqsize !  getmem getmem 193 reqsize ! getmem result @ 0= abort" t110.4";
+
+\ Allocating a block of memory twice should not yield the same pointer.
+
+variable q
+: s		pool p !  /pool reqsize !  fmtmem  pool poolbase !
+		256 reqsize !  getmem  p @ q ! ;
+: t120.1	s  256 reqsize !  getmem  result @ abort" t120.1" ;
+: t120.2	s  256 reqsize !  getmem  p @ q @ xor 0= abort" t120.2" ;
+
+\ If we allocate two blocks of 256 bytes, then free the first one,
+\ allocating a 512-byte block should appear after the 2nd block,
+\ not at the smaller hole.
+
+variable r
+: s		pool p !  /pool reqsize !  fmtmem  pool poolbase !
+		256 reqsize !  getmem  result @ abort" t130-s1"  p @ q !
+		getmem result @ abort" t130-s2"  p @ r !
+		q @ p ! relmem result @ abort" t130-s3" ;
+
+: t130.1	s  257 reqsize !  getmem  result @ abort" t130.1" ;
+: t130.2 	s  257 reqsize !  getmem  p @ r @ u<= abort" t130.2" ;
+
+\ If we allocate 5 blocks of 64 bytes each, then free the first four,
+\ allocating a 256 byte block _should_ be possible through de-fragmentation.
+
+variable u
+variable v
+
+: s		pool /pool $CC fill  pool p !  /pool reqsize !  fmtmem  pool poolbase !
+		64 reqsize !
+		  getmem result @ abort" t140-s1" p @ q !
+		  getmem result @ abort" t140-s2" p @ r !
+		  getmem result @ abort" t140-s3" p @ u !
+		  getmem result @ abort" t140-s4" p @ v !
+	  	  getmem result @ abort" t140-s5"
+		q @ p ! relmem  r @ p ! relmem  u @ p ! relmem  v @ p ! relmem ;
+: t140.1	s  256 reqsize !  getmem  result @ abort" t140.1" ;
+: t140.2	s  256 reqsize !  getmem  p @  q @  xor abort" t140.2" ;
+
+: t		." 100" cr t100.1 t100.2 t100.3
+		." 110" cr t110.1 t110.2 t110.3 t110.4
+		." 120" cr t120.1 t120.2 
+		." 130" cr t130.1 t130.2 
+		." 140" cr t140.1 t140.2 ;
+
+\ t
+
