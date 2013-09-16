59fdf55b762b8d8ef5bc22ee5973ceb9658d71dd
Samuel A. Falvo II <kc5tja@arrl.net>
2013-09-15 19:15:32 -0700

    Reduce code size.
    Frame-relative references consume a lot of code space.  This patch replaces
    frame-relative parameter passing with parameters passed via global variables.
    This reduced the (current) STS image from 1732 bytes to 1272, saving 460 bytes,
    or just about 26% of space.  Frame-relative parameters will still be necessary
    when it's time to pass parameters between transient program modules and the
    operating system.  However, this can be done only once (at the program/OS
    interface boundary), which will consume much less memory than simply using it
    everywhere.
    
    A pity, really -- frame-relative addressing is very convenient.

--- cores/Kestrel-2/sw/sts/SYS.fs
+++ cores/Kestrel-2/sw/sts/SYS.fs
@@ -8,37 +8,45 @@ include constants.fs
 $0400 origin
 defer, coldstart
 
+\ Global state for all OS services.
+
+int, rsn
+
 :, halt,	again, ;,
 :, crash	$C000 #, !,  $C000 #, @, -1 #, xor, $C050 #, !,  $AAAA #, $C0A0 #, !, ;,
 :, 0=		if, 0 #, exit, then, -1 #, ;,
 
 \ fillRect draws very simple rectangular patterns on the screen.
 \ This can be used to clear the screen, or to let the user in on the current progress of something.
-\ %fp points to the drawing control block.  It's fields are as follows:
-\	+0	return PC
-\	+1	Current bitmap pointer ($C000 <= p < $FE80 /\ (p & 1) == 0)
-\	+2	WORDS per row to draw
-\	+3	Residue to add to bitmap pointer after completing a row.  Beware of invariants.
-\	+4	Number of rows to draw.
-\	+5	Word to paste onto the bitmap.
-\	+6	(Internal) Word counter
-\	+7	(Internal) Row counter
 \ This procedure does not return anything.
 
+int, bitmapptr
+int, wrdperrow
+int, rowendres
+int, rowperbox
+int, bitmapdat
+int, wrdctr		( internal )
+int, rowctr		( internal )
+
 :, fillrow
-	6 +fp@,
-	if,	5 +fp@, 1 +fp@, !,
-		1 +fp@, 2 #, +, 1 +fp!,
-		6 +fp@, -1 #, +, 6 +fp!,
+	wrdctr @,
+	if,	bitmapdat @, bitmapptr @, !,
+		bitmapptr @, 2 #, +, bitmapptr !,
+		wrdctr @, -1 #, +, wrdctr !,
 		again,
 	then,
-	3 +fp@, 1 +fp@, +, 1 +fp!, ;,
+	rowendres @, bitmapptr @, +, bitmapptr !, ;,
 
 :, fillrows
-	7 +fp@, if, 2 +fp@, 6 +fp!, fillrow  7 +fp@, -1 #, +, 7 +fp!, again, then, ;,
+	rowctr @,
+	if,	wrdperrow @, wrdctr !,
+		fillrow
+		rowctr @, -1 #, +, rowctr !,
+		again,
+	then, ;,
 
-sub: fillRect
-	4 +fp@, 7 +fp!, fillrows  rfs,
+:, fillRect
+	rowperbox @, rowctr !, fillrows  ;,
 
 \ findTag can be used to locate a Tag structure anywhere in RAM.
 \ A Tag structure consists of several fields designed to help it stand out from random data and program data.
@@ -59,33 +67,35 @@ sub: fillRect
 \	+3	undefined
 \	+4	Reason code (non-zero)
 
-sub: findTag
-	2 +fp@, -1 #, xor, 1 #, +,  1 +fp@, +, $8000 #, and,
-	if,	1 +fp@, 0 #, +, @,
-		1 +fp@, 2 #, +, @, +,
-		1 +fp@, 4 #, +, @, +,
-		1 +fp@, 6 #, +, @, +,
+int, fndtagend
+int, fndtagsta
+
+:, findTag
+	fndtagend @, -1 #, xor, 1 #, +,  fndtagsta @, +, $8000 #, and,
+	if,	fndtagsta @, 0 #, +, @,
+		fndtagsta @, 2 #, +, @, +,
+		fndtagsta @, 4 #, +, @, +,
+		fndtagsta @, 6 #, +, @, +,
 		$FFFF #, xor,
-		if,  1 +fp@, 2 #, +, 1 +fp!,  ' findTag >BODY @ #, go,
+		if,  fndtagsta @, 2 #, +, fndtagsta !,  again,
 		then,
 
-		1 +fp@, @,  $C0DE #, xor,
-		if,  1 +fp@, 2 #, +, 1 +fp!,  ' findTag >BODY @ #, go,
+		fndtagsta @, @,  $C0DE #, xor,
+		if,  fndtagsta @, 2 #, +, fndtagsta !,  again,
 		then,
 
-		1 +fp@, 2 #, +, @,  1 +fp@,  xor,
-		if,  1 +fp@, 2 #, +, 1 +fp!,  ' findTag >BODY @ #, go,
+		fndtagsta @, 2 #, +, @,  fndtagsta @,  xor,
+		if,  fndtagsta @, 2 #, +, fndtagsta !,  again,
 		then,
 
-		1 +fp@, 4 #, +, @,  $DA7A #, xor,
-		if,  1 +fp@, 2 #, +, 1 +fp!,  ' findTag >BODY @ #, go,
+		fndtagsta @, 4 #, +, @,  $DA7A #, xor,
+		if,  fndtagsta @, 2 #, +, fndtagsta !,  again,
 		then,
 
-		1 +fp@, 3 +fp!,  0 #, 4 +fp!,
-		rfs,
+		0 #, rsn !,
+		exit,
 	then,
-	0 #, 3 +fp!,  ENOTFOUND 4 +fp!,
-	rfs,
+	ENOTFOUND rsn !, ;,
 
 \ fmtmem formats a memory pool with the required metadata to support allocation requests.
 \ Arguments:
@@ -101,115 +111,104 @@ sub: findTag
  4 const, /node
  3 const, roundup
 
-int, poolbase
-int, p
+int, mplsta
+int, mplsiz
 
-sub: fmtmem
-	1 +fp@, 1 +fp@, !,
-	2 +fp@, -/node and, -/node +, 1 +fp@, 2 #, +, !,
-	rfs,
+:, fmtmem
+	mplsta @, mplsta @, !,
+	mplsiz @, -/node and, -/node +, mplsta @, 2 #, +, !,  ;,
 
-\ getmem allocates a block of memory from a formatted memory pool.  The poolbase
+\ getmem allocates a block of memory from a formatted memory pool.  The mplsta
 \ variable must point to the pool to allocate from.
-\ Arguments:
-\	+0	Return PC
-\	+1	Size to allocate, in bytes
-\ If successful, it returns:
-\	+2	Pointer to allocated block of memory
-\	+3	0
-\ Otherwise:
-\	+2	undefined
-\	+3	non-zero reason code.
+
+int, p
+int, q
+
+int, memptr
+int, memsiz
 
 :, gm2
-	-1 +fp@, /node +, 1 +fp@, +, p !,  -1 +fp@, @,  p @, !,
-	-1 +fp@, /node +, 1 +fp@, +,   -1 +fp@, !,
-	1 +fp@, /node +, -1 #, xor, 1 #, +,  -1 +fp@, 2 #, +, @, +,  -1 +fp@, @, 2 #, +, !,
-	1 +fp@, 1 #, xor, -1 +fp@, 2 #, +, !,
-	-1 +fp@, /node +, 2 +fp!,  0 #, 3 +fp!,
+	memptr @, /node +, memsiz @, +, p !,  memptr @, @,  p @, !,
+	memptr @, /node +, memsiz @, +,   memptr @, !,
+	memsiz @, /node +, -1 #, xor, 1 #, +,  memptr @, 2 #, +, @, +,  memptr @, @, 2 #, +, !,
+	memsiz @, 1 #, xor, memptr @, 2 #, +, !,
+	memptr @, /node +, memptr !,  0 #, rsn !,
 	rfs,
 
 :, gmCombine
-	-1 +fp@, @, 2 #, +, @, 1 #, and, if, exit, then,
-	-1 +fp@, @, poolbase @, xor, if,
-		-1 +fp@, 2 #, +, -3 +fp!,
-		-1 +fp@, @, 2 #, +, @, /node +, -3 +fp@, @, +, -3 +fp@, !,
-		-1 +fp@, @, @, -1 +fp@, !,
+	memptr @, @, 2 #, +, @, 1 #, and, if, exit, then,
+	memptr @, @, mplsta @, xor, if,
+		memptr @, 2 #, +, q !,
+		memptr @, @, 2 #, +, @, /node +, q @, @, +, q @, !,
+		memptr @, @, @, memptr @, !,
 	then, ;,
 
 :, gm1
-	-1 +fp@, 2 #, +, @, -2 +fp!,
-
-	-2 +fp@,  1 #, and,
-	if,	-1 +fp@, @, -1 +fp!,
-		-1 +fp@, poolbase @, xor, if, again, then,
-		0 #, 2 +fp!,  ESIZE $100 #, +, 3 +fp!, rfs,
+	memptr @, 2 #, +, @, 1 #, and,
+	if,	memptr @, @, memptr !,
+		memptr @, mplsta @, xor, if, again, then,
+		0 #, memptr !,  ESIZE $100 #, +, rsn !, rfs,
 	then,
 
 	gmCombine
 
-	-1 +fp@, 2 #, +, @, -2 +fp!,
+	memptr @, 2 #, +, @, q !,
 
-	1 +fp@,  -1 #, xor, 1 #, +,  -2 +fp@, +,  $8000 #, and,
-	if,	-1 +fp@, @, -1 +fp!,
-		-1 +fp@, poolbase @, xor, if, again, then,
-		0 #, 2 +fp!,  ESIZE $500 #, +, 3 +fp!, rfs,
+	memsiz @,  -1 #, xor, 1 #, +,  q @, +,  $8000 #, and,
+	if,	memptr @, @, memptr !,
+		memptr @, mplsta @, xor, if, again, then,
+		0 #, memptr !,  ESIZE $500 #, +, rsn !, rfs,
 	then,
 
-	/node /node +, 1 +fp@, +, -1 #, xor, 1 #, +,  -2 +fp@, +, $8000 #, and, $8000 #, xor,
+	/node /node +, memsiz @, +, -1 #, xor, 1 #, +,  q @, +, $8000 #, and, $8000 #, xor,
 	if,	gm2
 	then,
 
-	-2 +fp@, 1 #, xor, -1 +fp@, 2 #, +, !,
-	-1 +fp@, /node +, 2 +fp!,  0 #, 3 +fp!,
+	q @, 1 #, xor, memptr @, 2 #, +, !,
+	memptr @, /node +, memptr !,  0 #, rsn !,
 	rfs, ;,
 
 sub: getmem
-	poolbase @,
-	if,	poolbase @, -1 +fp!,
-		1 +fp@, roundup +, -/node and, 1 +fp!,
+	mplsta @,
+	if,	mplsta @, memptr !,
+		memsiz @, roundup +, -/node and, memsiz !,
 		gm1
 	then,
-	0 #, 2 +fp!,  EBASE 3 +fp!,
+	0 #, memptr !,  EBASE rsn !,
 	rfs,
 
 \ relmem is used to release previously allocated memory, as returned by getmem.
-\ Arguments:
-\	+0	Return PC
-\	+1	Block of memory to release, as returned by getmem.
 \ Nothing is returned.
 
-sub: relmem
-	1 +fp@,  -/node +,  1 +fp!,
-	1 +fp@, 2 #, +, @,  -/node and,  1 +fp@, 2 #, +, !,
-	rfs,
+:, relmem
+	memptr @,  -/node +,  memptr !,
+	memptr @, 2 #, +, @,  -/node and,  memptr @, 2 #, +, !,  ;,
 
 \ First instruction of STS starts here.  coldstart vectors here.
 
 :, cold		$4000 #, %fp !,			( Initialize the STS frame pointer )
 		-8 fp+!,			( Let user know we're running )
-			$C000 #, 1 +fp!,
-			40 #, 2 +fp!,
-			80 #, 3 +fp!,
-			100 #, 4 +fp!,
-			$0000 #, 5 +fp!,
+			$C000 #, bitmapptr !,
+			40 #, wrdperrow !,
+			80 #, rowendres !,
+			100 #, rowperbox !,
+			$0000 #, bitmapdat !,
 			fillRect
 
-			0 #, 1 +fp!,		( locate free memory pool )
-			$3000 #, 2 +fp!,
+			0 #, fndtagsta !,	( locate free memory pool and initialize it. )
+			$3000 #, fndtagend !,
 			findTag
-			4 +fp@, if,		( If not found... )
-				$C000 #, 1 +fp!,	( indicate error to user and halt )
-				40 #, 2 +fp!,
-				0 #, 3 +fp!,
-				200 #, 4 +fp!,
-				$FFFF #, 5 +fp!,
+			rsn @, if,		( If not found... )
+				$C000 #, bitmapptr !,	( indicate error to user and halt )
+				40 #, wrdperrow !,
+				0 #, rowendres !,
+				200 #, rowperbox !,
+				$FFFF #, bitmapdat !,
 				fillRect
 				halt,
 			then,
-			3 +fp@, $C000 #, +, 1 +fp!,
-			3 +fp@, $C000 #, +, poolbase !,
-			3 +fp@, -1 #, xor, 1 #, +, $4000 #, +, 2 +fp!,
+			fndtagsta @, $C000 #, +, mplsta !,
+			fndtagsta @, -1 #, xor, 1 #, +, $4000 #, +, mplsiz !,
 			fmtmem
 
 \			$C000 #, 1 +fp!,	( Memory pool updated.  Time to mount our filesystem. )
@@ -219,34 +218,34 @@ sub: relmem
 \			$0000 #, 5 +fp!,
 \			fillRect
 
-			256 #, 1 +fp!,
+			256 #, memsiz !,
 			getmem
-			3 +fp@,
+			rsn @,
 			if,	$F640 #, crash halt,
 			then,
-			2 +fp@, 0=
+			memptr @, 0=
 			if,	$F642 #, crash halt,
 			then,
-			2 +fp@, -3 +fp!,
-			256 #, 1 +fp!,
+			memptr @, -1 +fp!,
+			256 #, memsiz !,
 			getmem
-			3 +fp@,
+			rsn @,
 			if,	$F644 #, crash halt,
 			then,
-			2 +fp@, -3 +fp@, xor, 0=
+			memptr @, -1 +fp@, xor, 0=
 			if,	$F646 #, crash halt,
 			then,
 
-			2 +fp@, 1 +fp!, relmem
-			-3 +fp@, 1 +fp!, relmem
+			relmem
+			-1 +fp@, memptr !, relmem
 
-			512 #, 1 +fp!,
+			512 #, memsiz !,
 			getmem
-			3 +fp@,
+			rsn @,
 			if,	$F740 #, crash halt,
 			then,
 
-			2 +fp@,  poolbase @, /node +, xor, crash
+			memptr @,  mplsta @, /node +, xor, crash
 		8 fp+!,
 
 		
