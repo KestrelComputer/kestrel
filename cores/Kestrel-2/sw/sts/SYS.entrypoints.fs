02f49a93cf1e432cc3740c5837409ee4b3177304
Samuel A. Falvo II <kc5tja@arrl.net>
2013-09-24 23:09:38 -0700

    Patch vector table at start of STS

--- cores/Kestrel-2/sw/sts/SYS.entrypoints.fs
+++ cores/Kestrel-2/sw/sts/SYS.entrypoints.fs
@@ -1,92 +1,117 @@
 \ These entry points thunk from frame-based parameter passing rules to global-variable rules used internally.
 
-sub: fndtag-epv
-	1 fp+@, fndtagsta !,
-	2 fp+@, fndtagend !,
+sub: fndtag-v
+	1 +fp@, fndtagsta !,
+	2 +fp@, fndtagend !,
 	findtag
-	fndtagsta @, 3 fp+!,
-	rsn @, 4 fp+!,
+	fndtagsta @, 3 +fp!,
+	rsn @, 4 +fp!,
 	rfs,
 
-sub: getmem-epv
-	1 fp+@, memsiz !,
+' fndtag-v >body @ is, fndtag-epv
+
+sub: getmem-v
+	1 +fp@, memsiz !,
 	getmem
-	memptr @, 2 fp+!,
-	rsn @, 3 fp+!,
+	memptr @, 2 +fp!,
+	rsn @, 3 +fp!,
 	rfs,
 
-sub: relmem-epv
-	1 fp+@, memptr !,
+' getmem-v >body @ is, getmem-epv
+
+sub: relmem-v
+	1 +fp@, memptr !,
 	relmem
 	rfs,
 
-sub: open-epv
-	1 fp+@, filnamptr !,
-	2 fp+@, filnamlen !,
+' relmem-v >body @ is, relmem-epv
+
+sub: open-v
+	1 +fp@, filnamptr !,
+	2 +fp@, filnamlen !,
 	open
-	filscb @, 3 fp+!,
-	rsn @, 4 fp+!,
+	filscb @, 3 +fp!,
+	rsn @, 4 +fp!,
 	rfs,
 
-sub: close-epv
-	1 fp+@, filscb !,
+' open-v >body @ is, open-epv
+
+sub: close-v
+	1 +fp@, filscb !,
 	close
 	rfs,
 
-sub: read-epv
-	1 fp+@, inpscb !,
-	2 fp+@, inpbuf !,
-	3 fp+@, inplen !,
+' close-v >body @ is, close-epv
+
+sub: read-v
+	1 +fp@, inpscb !,
+	2 +fp@, inpbuf !,
+	3 +fp@, inplen !,
 	read
-	inpcnt @, 4 fp+!,
-	rsn @, 5 fp+!,
+	inpcnt @, 4 +fp!,
+	rsn @, 5 +fp!,
 	rfs,
 
-sub: loadseg-epv
-	1 fp+@, filnamptr !,
-	2 fp+@, filnamlen !,
+' read-v >body @ is, read-epv
+
+sub: loadseg-v
+	1 +fp@, filnamptr !,
+	2 +fp@, filnamlen !,
 	loadseg
-	segptr @, 3 fp+!,
-	rsn @, 4 fp+!,
+	segptr @, 3 +fp!,
+	rsn @, 4 +fp!,
 	rfs,
 
-sub: unloadseg-epv
-	1 fp+@, segptr !,
+' loadseg-v >body @ is, loadseg-epv
+
+sub: unloadseg-v
+	1 +fp@, segptr !,
 	unloadseg
 	rfs,
 
-sub: exec-epv
-	1 fp+@, filnamptr !,
-	2 fp+@, filnamlen !,
-	3 fp+@, parambuf !,
+' unloadseg-v >body @ is, unloadseg-epv
+
+sub: exec-v
+	1 +fp@, filnamptr !,
+	2 +fp@, filnamlen !,
+	3 +fp@, parambuf !,
 	exec
-	rsn @, 4 fp+!,
+	rsn @, 4 +fp!,
 	rfs,
 
-sub: exit.-epv
+' exec-v >body @ is, exec-epv
+
+sub: exit.-v
 	exit.
 	rfs,
 
-sub: compare-epv
-	1 fp+@, strptr1 !,
-	2 fp+@, strlen1 !,
-	3 fp+@, strptr2 !,
-	4 fp+@, strlen2 !,
+' exit.-v >body @ is, exit.-epv
+
+sub: compare-v
+	1 +fp@, strptr1 !,
+	2 +fp@, strlen1 !,
+	3 +fp@, strptr2 !,
+	4 +fp@, strlen2 !,
 	compare
-	strres @, 5 fp+!,
+	strres @, 5 +fp!,
 	rfs,
 
-sub: strdif-epv
-	1 fp+@, strptr1 !,
-	2 fp+@, strptr2 !,
+' compare-v >body @ is, compare-epv
+
+sub: strdif-v
+	1 +fp@, strptr1 !,
+	2 +fp@, strptr2 !,
 	strdif
-	strres @, 3 fp+!,
+	strres @, 3 +fp!,
 	rfs,
 
-sub: movmem-epv
-	1 fp+@, strptr1 !,
-	2 fp+@, strptr2 !,
-	3 fp+@, strlen1 !,
-	memmov
+' strdif-v >body @ is, strdif-epv
+
+sub: movmem-v
+	1 +fp@, strptr1 !,
+	2 +fp@, strptr2 !,
+	3 +fp@, strlen1 !,
+	movmem
 	rfs,
 
+' movmem-v >body @ is, movmem-epv
