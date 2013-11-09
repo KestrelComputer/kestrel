e77a9f9dc0d7e25eca874d0940da546cea944c13
Samuel A. Falvo II <kc5tja@arrl.net>
2013-11-09 09:59:13 -0800

    Simplify STS shell a bit.
    Call into the textiface library to clear screen, etc.

--- cores/Kestrel-2/sw/sts/PRG.start.fs
+++ cores/Kestrel-2/sw/sts/PRG.start.fs
@@ -8,7 +8,18 @@ create, lib.textiface
 int, textifaceBase
 include INC.textiface.fs
 
+160 const, /cmdbuf
+create, cmdbuf
+	char S c,,	char Y c,,	char S c,,	char : c,,
+	char p c,,	char r c,,	char g c,,	char . c,,
+160 allot,
+
 int, p
+int, #cmdbuf
+int, psch
+int, pdch
+int, n
+
 
 :, stop
 	$C000 #, p !,
@@ -16,7 +27,100 @@ int, p
 	while,	$FFFF #, p @, !,
 		p @, 2 #, +, p !,
 	repeat,
+	4 +fp@, $C002 #, !,
+	4 +fp@, -1 #, xor, $C052 #, !,
+	$AAAA #, $C0A2 #, !,
 	begin, again,
+;,
+
+:, cls
+	0 #, 1 +fp!,  0 #, 2 +fp!,
+	80 #, 3 +fp!,  25 #, 4 +fp!,
+	BlackRect
+;,
+
+create, prompt$
+	C", > "
+
+create, banner$
+	C", STS/K2 V1"
+
+:, prompt
+	prompt$ 1 #, +, 1 +fp!,
+	prompt$ c@, 2 +fp!,
+	PrintStr
+;,
+
+:, getcmd
+	prompt 
+	Accept
+	1 +fp@, #cmdbuf !,
+;,
+
+:, skipws
+	$FF00 #, psch !,
+        begin,  psch @, -$FF00 #, +, $8000 #, and,
+                if,     exit,
+                then,
+                
+                psch @, -$FFA0 #, +, $8000 #, and, $8000 #, xor,
+                if,     exit,
+                then,
+                
+                psch @, c@, -33 #, +, $8000 #, and, $8000 #, xor,
+                if,     exit,
+                then,
+                
+                psch @, 1 #, +, psch !,
+        again,
+;,
+
+:, copy2ws
+	cmdbuf 4 #, +, pdch !,
+        8 #, n !,
+        begin,  psch @, -$FF00 #, +, $8000 #, and,
+                if,     exit,
+                then,
+                
+                psch @, -$FFA0 #, +, $8000 #, and, $8000 #, xor,
+                if,     exit,
+                then,
+                
+                n @, -52 #, +, $8000 #, and, $8000 #, xor,
+                if,     exit,
+                then,
+                
+		n @, #cmdbuf @, -1 #, xor, +, 1 #, +, $8000 #, and, $8000 #, xor,
+		if,	exit,
+		then,
+
+                psch @, c@, -33 #, +, $8000 #, and,
+                if,     exit,
+                then,
+                
+                psch @, c@, pdch @, c!,
+
+                psch @, 1 #, +, psch !,
+                pdch @, 1 #, +, pdch !,
+                n @, 1 #, +, n !,
+        again,
+;,
+
+:, run
+	cmdbuf 1 +fp!,
+	n @, 2 +fp!,
+	PrintStr CR
+;,
+
+:, try
+	CR
+	skipws copy2ws run
+;,
+
+:, banner
+	banner$ 1 #, +, 1 +fp!,
+	banner$ c@, 2 +fp!,
+	PrintStr CR
 ;,
 
 :, main
@@ -31,13 +135,10 @@ int, p
 	then,
 	3 +fp@, textifaceBase !,
 
-	0 #, 1 +fp!,
-	0 #, 2 +fp!,
-	80 #, 3 +fp!,
-	25 #, 4 +fp!,
-	BlackRect
-
-	begin,	
+	cls
+	banner
+	begin,	getcmd
+		try
 	again,
 ;,
 
