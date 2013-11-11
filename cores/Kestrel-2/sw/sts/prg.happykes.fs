2a5ccb596f34d3736e1f11a3520fd4f2750677d0
Samuel A. Falvo II <kc5tja@arrl.net>
2013-11-11 00:31:34 -0800

    Bring happykes back as an STS command
    This program demonstrates the exec/exit relationship.  STS shell invokes exec
    to run happykes, and happykes invokes exit to return to the STS shell.

--- cores/Kestrel-2/sw/sts/prg.happykes.fs
+++ cores/Kestrel-2/sw/sts/prg.happykes.fs
@@ -1,5 +1,7 @@
 defer, happyboot
 
+include INC.STS.SYS.fs
+
 int, p
 int, q
 
@@ -21,16 +23,41 @@ create, happyicon
 0000000000000000 ,,
 0000000000000000 ,,
 0000000000000000 ,,
+
+create, sadicon
+0001100000011000 ,,
+0111111001111110 ,,
+1000000000000001 ,,
+0001100000011000 ,,
+0000100000001000 ,,
+0000000010000000 ,,
+0000000100000000 ,,
+0000001100000000 ,,
+0000000110000000 ,,
+0000000000000000 ,,
+0000001111000000 ,,
+0000111111110000 ,,
+0011000000001100 ,,
+0000000000000000 ,,
+0000000000000000 ,,
+0000000000000000 ,,
 decimal
+
+$B000 const, kqstat
+$B002 const, kqdata
 
 :, z		0 #, q @, !, ;,
 :, cls0		q @, if, z q @, 2 #, +, q !, again, then, ;,
 :, cls		$C000 #, q !, cls0 ;,
 :, r		q @, @, p @, !,  p @, 80 #, +, p !,  q @, 2 #, +, q !, ;,
 :, 16r		r r r r  r r r r  r r r r  r r r r ;,
-:, icon		$D334 #, p !, happyicon q !, 16r ;,
-:, deadlock	again, ;,
-:, happy 	cls icon deadlock ;,
+:, yay		$D334 #, p !, happyicon q !, 16r ;,
+:, boo		$D334 #, p !, sadicon q !, 16r ;,
+:, wait		kqstat c@, 1 #, and, if, again, then, ;,
+:, d		kqdata c!, ;,
+:, drain	d d d d d d d d d d d d d d d d d ;,
+:, waitkey	drain wait drain ;,
+:, happy 	-4 fp+!, cls yay waitkey boo exit. ;,
 ' happy >body @ is, happyboot
-reloc" imgs/prg.start"
+reloc" imgs/prg.happy"
 
