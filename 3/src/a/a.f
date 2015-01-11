50a59a0916d2d63be7eb158f403634048bd6ceb8
Samuel A. Falvo II <sam.falvo@rackspace.com>
2015-01-11 13:24:13 -0800

    Implement source file iteration and error handling.
    It will likely need revision later on when we consider incorporating
    line numbers.

---
+++ 3/src/a/a.f
@@ -0,0 +1,16 @@
+\ Polaris V0.1 Assembler V0.1
+\ vim: set ts=8 sw=8 noet ai:
+
+ONLY FORTH DEFINITIONS
+VOCABULARY asm64
+ALSO asm64 DEFINITIONS
+
+INCLUDE errors
+INCLUDE main
+
+ONLY FORTH DEFINITIONS
+ALSO asm64
+
+main
+bye
+
