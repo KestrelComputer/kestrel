9e08e08d7b84d74132e1c9fc6e200b074051202b
Samuel A. Falvo II <kc5tja@arrl.net>
2015-02-16 18:12:27 -0800

    WIP for xfer to laptop

--- 3/src/mlm/bios.i
+++ 3/src/mlm/bios.i
@@ -3,4 +3,13 @@
 
 0 	DUP CONSTANT	brod_uart_base
 8 +	DUP CONSTANT	brod_initsp
-	CONSTANT	brod_sizeof
+8 +	DUP CONSTANT	brod_bcb
+8 +	CONSTANT	brod_sizeof
+
+
+\ BIOS maintains state in RAM as well.  For lack of better name, this is called
+\ the BIOS Control Block, or BCB.
+
+0	DUP CONSTANT	bcb_keypress
+1 +	CONSTANT	bcb_sizeof
+
--- 3/src/mlm/code.asm
+++ 3/src/mlm/code.asm
@@ -7,8 +7,17 @@
 	ZERO	CHAR *		A0	ADDI
 	JAL> bios_putchar	RA	JAL
 
+-> do-it-again
+
 	ZERO	banner		A0	ADDI
 	JAL> bios_putstrz	RA	JAL
+
+-> wait-for-key
+	JAL> bios_getchar	RA	JAL
+	X0	A0		wait-for-key BEQ
+	JAL> bios_getchar	RA	JAL
+	JAL> bios_putchar	RA	JAL
+	wait-for-key		X0	JAL
 
 	LC			ZERO	JAL
 
@@ -24,6 +33,8 @@
 	X0 brod_uart_base	T0	LD
 	A0		T0 UART_TX	SB
 	RA 0			X0	JALR
+
+
 
 \ Print a zero-terminated string to the user's console.
 \ This routine makes no attempt to interpret terminal control codes.
@@ -49,4 +60,37 @@
 	SP 0			S0	LD
 	SP	16		SP	ADDI
 	RA 0			X0	JALR
+
+
+\ Check to see if a key has been pressed.
+\ Returns pending ASCII code if a key is pending; use bios_getchar to pop it.
+\ Returns zero otherwise.
+
+-> bios_chkchar ( ) \ ascii
+	X0 brod_bcb				T0			LD	( T0 -> BIOS Control Block )
+	T0 bcb_keypress				A0			LBU	( do we have a key pending? )
+	X0			A0		B> .bios.chkchar.done	BNE	( Yes, just return it )
+	X0 brod_uart_base			T1			LD	( T1 -> UART base )
+	T1 UART_RX				A0			LBU	( Check UART receive register )
+	A0					T0 bcb_keypress		SB	( Hopefully queue key pending buffer )
+-> .bios.chkchar.done
+	RA 0					X0			JALR
+
+
+\ Get a character, if it's been pressed.
+\ Returns the current ASCII code if pending; zero otherwise.
+\ Note that this function does not block.
+\ To non-destructively read the next byte, if any, use bios_chkchar.
+
+-> bios_getchar ( ) \ ascii
+	X0 brod_bcb				T0			LD	( T0 -> BIOS Control Block )
+	X0 brod_uart_base			T1			LD	( T1 -> UART )
+	T0 bcb_keypress				A0			LBU	( pending keypress? )
+	X0			A0		B> .bios.getchar.q	BNE	( Yes, pop the queue )
+	T1 UART_RX				A0			LBU	( otherwise, read directly from uart )
+	RA 0					X0			JALR
+
+-> .bios.getchar.q
+	X0					T0 bcb_keypress		SB
+	RA 0					X0			JALR
 
