ef12903f8e42bdc78233cd5e94805557328c66cf
Samuel A. Falvo II <kc5tja@arrl.net>
2015-09-06 18:16:50 -0700

    Fix spurious DROP at start of each colon def

--- 3/src/mf/mf.fs
+++ 3/src/mf/mf.fs
@@ -1,3 +1,17 @@
 S" pass1.fs" included
 
+\ The new colon compiler starts here.  It's technically part of
+\ pass 1 implementation, but ; drives subsequent passes as well.
 
+host definitions
+
+: :0		buf0 fi0 ni0 label0 ;
+: :,		:0 bl word drop ;
+: ;,		rfs, ;
+
+target definitions
+
+:: :		:, ;;
+:: ;		;, ;;
+
+host definitions
