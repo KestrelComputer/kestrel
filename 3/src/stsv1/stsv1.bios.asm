a717de79751a5cf0cbd97e496bc3a52e84699c95
Samuel A. Falvo II <kc5tja@arrl.net>
2016-01-01 20:29:21 -0800

    Accept keyboard input from the emulated keyboard.
    It's not Unix anymore, so you have to use CTRL-J to enter a line-feed,
    and CTRL-H to backspace.  But, it now accepts true ASCII input via the
    BIOS!
    
    Fixes #180

--- 3/src/stsv1/stsv1.bios.asm
+++ 3/src/stsv1/stsv1.bios.asm
@@ -16,3 +16,37 @@ emit:		addi	rsp, rsp, -32
 		ld	a2, 24(rsp)
 		addi	rsp, rsp, 32
 		jalr	x0, 0(ra)
+
+caninp:		addi	rsp, rsp, -24
+		sd	ra, 0(rsp)
+		sd	a0, 8(rsp)
+		sd	a1, 16(rsp)
+
+		addi	a0, x0, 2
+		ld	a1, bd_jumptab(x0)
+		jalr	ra, BIOS_CANINP(a1)
+		addi	dsp, dsp, -8
+		sd	a0, 0(dsp)
+
+		ld	ra, 0(rsp)
+		ld	a0, 8(rsp)
+		ld	a1, 16(rsp)
+		addi	rsp, rsp, 24
+		jalr	x0, 0(ra)
+
+chrinp:		addi	rsp, rsp, -24
+		sd	ra, 0(rsp)
+		sd	a0, 8(rsp)
+		sd	a1, 16(rsp)
+
+		addi	a0, x0, 2
+		ld	a1, bd_jumptab(x0)
+		jalr	ra, BIOS_CHRINP(a1)
+		addi	dsp, dsp, -8
+		sd	a0, 0(dsp)
+
+		ld	ra, 0(rsp)
+		ld	a0, 8(rsp)
+		ld	a1, 16(rsp)
+		addi	rsp, rsp, 24
+		jalr	x0, 0(ra)
