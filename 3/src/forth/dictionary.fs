6b4a01d22537ed04c35a46299428a59a8abcff78
Samuel A. Falvo II <kc5tja@arrl.net>
2016-03-30 00:29:06 -0700

    tfind to locate words in target dictionary.

--- 3/src/forth/dictionary.fs
+++ 3/src/forth/dictionary.fs
@@ -23,13 +23,15 @@ $FFFFFFFFFFF00000 constant RomBase
 1048576 constant /dict		\ space to store compiled code and headers
 create dict
   /dict allot
+dict /dict + constant dictEnd
 variable dictp			\ ptr to next byte to compile code or data to
 variable headp			\ ptr to last created dictionary header
+variable tcontext		\ ptr to last defined header that's visible
 variable dictOrg		\ base address for the dictionary image
 
 : torg			dictOrg ! ;
 : safe			dictp @ headp @ u>= abort" out of header space" ;
-: dict0			dict dictp !  dict /dict + headp ! ;
+: dict0			dict dictp !  dictEnd headp !  dictEnd tcontext ! ;
 : h>t ( a - ta )	dict - dictOrg @ + ;
 : t>h ( ta - a )	dictOrg @ - dict + ;
 
@@ -67,10 +69,21 @@ RomBase torg			\ Kestrel-3's ROM sits at -1MB.
 \ The new word will be "smudged" though, since the
 \ search context will not have been updated yet.
 
+
 : round			CellMask + -/cell and ;
 : dec ( a u - a u )	dup 1+ round negate headp +! ;
 : place ( a u - )	dup headp @ c!  headp @ 1+ swap move ;
 : tname, ( a u - )	dec safe place ;
 : th, ( n - )		-/cell headp +!  safe  headp @ ! ;
 : thead, ( cfa a u - )	tname, there th, th, ;
+: treveal		headp @ tcontext ! ;
+
+: skip ( a - a' )	2 cells + dup c@ round + ;
+: 3dup ( abc - abcabc)	>r 2dup r@ -rot r> ;
+: tfind ( a u - x T | a u F )
+  tcontext @ begin
+    dup dictEnd u>= if drop 0 exit then
+    3dup 2 cells + count compare 0= if nip nip -1 exit then
+    skip
+  again ;
 
