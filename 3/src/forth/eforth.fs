816281f1103413dc86e45e1871963f6a8dae0217
Samuel A. Falvo II <kc5tja@arrl.net>
2016-04-03 13:03:02 -0700

    High-level words on pg 19 of eForth guide

--- 3/src/forth/eforth.fs
+++ 3/src/forth/eforth.fs
@@ -17,6 +17,19 @@
 \ high-level dependencies.  These exceptions are typically
 \ related to IRQ and reset entry points.
 
+t: ?DUP		DUP IF DUP THEN ;
+t: ROT		>R SWAP R> SWAP ;
+t: 2DROP	DROP DROP ;
+t: 2DUP		OVER OVER ;
+t: +		UM+ DROP ;
+t: NOT		-1 XOR ;
+t: INVERT	-1 XOR ;
+t: NEGATE	-1 XOR 1 + ;
+t: DNEGATE	NOT >R NOT 1 UM+ R> + ;
+t: D+		>R SWAP >R UM+ R> R> + + ;
+t: -		NEGATE + ;
+t: ABS		DUP 0< IF NEGATE THEN ;
+
 \ !io is responsible for initializing all the I/O devices
 \ for Forth to run.  This includes clearing the keyboard
 \ queue, initializing the video display hardware, etc.
