c9a3b22082bad4c4f8e0d75d2cdb5942cb220a35
Samuel A. Falvo II <sam.falvo@rackspace.com>
2016-04-05 22:18:08 -0700

    For eForth compat, (dolit)->doLIT; pad names w/ 0s

--- 3/src/forth/dictionary.fs
+++ 3/src/forth/dictionary.fs
@@ -126,7 +126,9 @@ rom0
 
 : round			CellMask + -/cell and ;
 : dec ( a u - a u )	dup 1+ round negate headp +! ;
-: place ( a u - )	dup headp @ c!	headp @ 1+ swap move ;
+: padded		headp @ over 8 + -8 and 0 fill ;
+			( 8 + instead of 7 + to acct. for length byte )
+: place ( a u - )	padded dup headp @ c! headp @ 1+ swap move ;
 : tname, ( a u - )	dec safe place ;
 : th, ( n - )		-/cell headp +!  safe  headp @ ! ;
 : thead, ( cfa a u - )	tname, tcontext @ th, there th, th, ;
