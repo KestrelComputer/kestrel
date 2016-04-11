80a12b342dbbcfa3e67c5c7663101e01ddcc45c5
Samuel A. Falvo II <kc5tja@arrl.net>
2016-04-11 07:59:18 -0700

    WIP VIBE block editor for Kestrel Forth

--- 3/src/forth/forth.fs
+++ 3/src/forth/forth.fs
@@ -6,6 +6,7 @@ S" kernel.fs" included	\ eForth primitives
 S" eforth.fs" included	\ eForth high-level words
 S" sdcard.fs" included	\ SD card I/O driver
 S" blockIO.fs" included	\ BLOCK, BUFFER, et. al.
+S" vibe.fs" included	\ Screen editor
 
 vectors
 S" forth.rom" save
