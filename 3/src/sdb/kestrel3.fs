03c2f425d32d68434e1c195d6033eb93d42bb261
Samuel A. Falvo II <kc5tja@arrl.net>
2016-04-16 22:27:41 -0700

    first commit

---
+++ 3/src/sdb/kestrel3.fs
@@ -0,0 +1,80 @@
+\ Wishbone bus-specific flags for various types of devices.
+$8F CONSTANT WB_RAMROM ( little-endian, all widths )
+$88 CONSTANT WB_GPIA2 ( little-endian, 64-bit access only )
+$01 CONSTANT WB_KIA ( 8-bit access only )
+
+\ Kestrel Computer Project vendor ID.  Chosen to be pseudo-
+\ random with low probability of collision.
+$C0C21E6D3B1EDF88 CONSTANT V_KESTREL
+
+\ Kestrel-2 and Kestrel-3 devices.
+1 CONSTANT D_MOTHERBOARD_E ( emulated motherboard )
+2 CONSTANT D_S16X4_CPU	( 16-bit CPU used in Kestrel-2 )
+3 CONSTANT D_E_RTS_CPU	( E Red Tail Subset CPU for Kestrel-3 )
+4 CONSTANT D_REDTAIL_CPU ( RV64-compliant CPU for Kestrel-3 )
+5 CONSTANT D_RAM	( generic RAM )
+6 CONSTANT D_GPIA1	( 16-bit GPIA for Kestrel-2 )
+7 CONSTANT D_GPIA2	( 64-bit GPIA for Kestrel-3 )
+8 CONSTANT D_KIA	( KIA for Kestrel-2/-3 )
+9 CONSTANT D_ROM	( generic ROM )
+
+\ Kestrel-3 device classes.  These actually set the ABI
+\ of a particular device.
+1 CONSTANT C_RV64IS_CORE	( 64-bit RISC-V CPU )
+2 CONSTANT C_RAM		( generic RAM )
+3 CONSTANT C_GPIA2		( 64-bit GPIA-2 )
+4 CONSTANT C_KIA_KBD		( 8-bit KIA-I or KIA-II )
+5 CONSTANT C_KIA_MOUSE		( 8-bit KIA-I or KIA-II )
+6 CONSTANT C_MGIA_FRAMEBUFFER	( framebuffer RAM )
+7 CONSTANT C_SDB_CONFIG_ROM	( ROM containing SDB data )
+8 CONSTANT C_BOOTSTRAP_ROM	( ROM containing boot code )
+
+\ Kestrel-3 Emulator definition
+B_WISHBONE interconnect(
+0 -1 range
+V_KESTREL D_MOTHERBOARD_E $00003000 version
+$20160416 date
+S" e-emulator" name
+
+	C_RV64IS_CORE 0 0 0 device
+	0 0 range
+	V_KESTREL D_E_RTS_CPU 0 version
+	$20160416 date
+	S" cpu" name
+
+	C_RAM 0 0 WB_RAMROM device
+	$000000 $FEFFFF range
+	V_KESTREL D_RAM 0 version
+	$20160416 date
+	S" ram" name
+
+	C_MGIA_FRAMEBUFFER 0 1 WB_RAMROM device
+	$FF0000 $FFFFFF range
+	V_KESTREL D_RAM 0 version
+	$20160416 date
+	S" framebuffer" name
+
+	C_GPIA2 0 0 WB_GPIA2 device
+	$0100000000000000 $010000000000000F range
+	V_KESTREL D_GPIA2 0 version
+	$20160416 date
+	S" gpia2" name
+
+	C_KIA_KBD 0 0 WB_KIA device
+	$0200000000000000 $0200000000000001 range
+	V_KESTREL D_KIA 1 version
+	$20160416 date
+	S" kia-sdl" name
+
+	C_SDB_CONFIG_ROM 0 0 WB_RAMROM device
+	$0300000000000000 $03000000000001FF range
+	V_KESTREL D_ROM 0 version
+	$20160416 date
+	S" sdb-config-rom" name
+
+	C_BOOTSTRAP_ROM 0 0 WB_RAMROM device
+	$FFFFFFFFFFF00000 $FFFFFFFFFFFFFFFF range
+	V_KESTREL D_ROM 0 version
+	$20160416 date
+	S" boot-rom" name
+)interconnect
---
+++ 3/src/sdb/sdb.fs
@@ -0,0 +1,85 @@
+\ This software is used to build SDB ROM images for the e
+\ emulator.  It is based on the SDB V1.1 specifications
+\ available here: http://www.ohwr.org/projects/sdb/wiki
+
+\ Kestrel-specific settings are based on the specifications
+\ available on the Kestrel Github page at:
+\ https://github.com/KestrelComputer/kestrel/wiki/SdbRegistry
+
+512 CONSTANT /rom
+CREATE rom /rom ALLOT
+VARIABLE romp
+
+: 0rom		rom /rom $FF FILL  0 romp ! ;
+0rom
+: romhere	romp @ ;
+: romallot	romp +! ;
+: h>r		rom - ;
+: r>h		rom + ;
+: ALIGN		romp @ OVER 1- + SWAP NEGATE AND romp ! ;
+: rc@		r>h C@
+: r@		r>h @ ;
+: rc!		r>h C! ;
+: r!		r>h ! ;
+
+VARIABLE becheck
+VARIABLE le	( true if little-endian; false otherwise )
+: checkbe	$1122 becheck ! becheck C@ $22 = le ! ;
+checkbe
+
+: hswap le @ IF DUP $FF AND 8 LSHIFT SWAP 8 RSHIFT $FF AND OR THEN ;
+: wswap le @ IF DUP hswap 16 LSHIFT SWAP 16 RSHIFT hswap OR THEN ;
+: dswap le @ IF DUP wswap 32 LSHIFT SWAP 32 RSHIFT wswap OR THEN ;
+
+: safe		romhere /rom U>= ABORT" SDB ROM too small" ;
+: (rc,)		safe DUP romhere rc!  1 romallot 8 RSHIFT ;
+: (rh,)		(rc,) (rc,) ;
+: (rw,)		(rh,) (rh,) ;
+: (r,)		(rw,) (rw,) ;
+: rc,		(rc,) DROP ;
+: rh,		2 ALIGN hswap (rh,) DROP ;
+: rw,		4 ALIGN wswap (rw,) DROP ;
+: r,		8 ALIGN dswap (r,) DROP ;
+
+: rh!		romhere >R romp ! rh, R> romp ! ;
+
+\ Constructor words covering interconnects and devices.
+VARIABLE 'interconnect
+: desc0		safe romhere r>h 64 0 FILL romhere 63 + rc! ;
+: interconnect( ( bustype - )
+  0 desc0
+  romhere 'interconnect ! S" SDB-" romhere r>h SWAP MOVE
+  4 romallot 0 rh, 1 rc, rc, ;
+: device ( class maj min busspec - )
+  1 desc0  2>R SWAP rh, rc, 2R> SWAP rc, rw, ;
+: range ( first last - )	SWAP r, r, ;
+: version ( vendor id vers - )	>R SWAP r, rw, R> rw, ;
+: date ( date - )		rw, ;
+CREATE namebuf 19 ALLOT
+: name ( a u - )
+  namebuf 19 32 FILL  0 MAX 19 MIN namebuf SWAP MOVE
+  namebuf romhere r>h 19 MOVE 20 romallot ;
+: )interconnect ( - )
+  romp @ 63 AND ABORT" Alignment error in interconnect"
+  romp @ 'interconnect @ - 6 RSHIFT
+  'interconnect @ 4 + rh! ;
+
+\ ROM image generator
+VARIABLE h
+: open		R/W BIN CREATE-FILE THROW h ! ;
+: write		rom romhere h @ .S CR WRITE-FILE .S CR ;
+: close		h @ CLOSE-FILE THROW ;
+: (save)	open write ;
+: save		13 PARSE (save) h @ CLOSE-FILE THROW THROW ;
+\ : save		13 PARSE ['] (save) CATCH h @ ?DUP IF CLOSE-FILE DROP THEN THROW ;
+
+\ Bus types
+0 CONSTANT B_WISHBONE
+1 CONSTANT B_SDBFS
+
+\ Bus-specific flags (Wishbone)
+$80 CONSTANT WB_LITTLE_ENDIAN
+$08 CONSTANT WB_64BIT
+$04 CONSTANT WB_32BIT
+$02 CONSTANT WB_16BIT
+$01 CONSTANT WB_8BIT
